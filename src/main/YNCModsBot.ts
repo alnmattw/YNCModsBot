91f2181520e906f475f3211f0569051278f5fc60
Alan Matthew <alnmattw@gmail.com>
2022-12-14 12:32:45 +0800

    more refactoring

---
+++ src/main/YNCModsBot.ts
@@ -0,0 +1,5 @@
+import { ModuleCollection } from '../modules/ModuleCollection'
+var moduleCollection = new ModuleCollection()
+moduleCollection.getModuleData()
+console.log(moduleCollection)
+console.log('hello world')
