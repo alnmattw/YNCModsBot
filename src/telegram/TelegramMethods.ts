57ef53b100996e43eeb363d6ff47eba03d8137c9
Alan Matthew <alnmattw@gmail.com>
2022-12-14 14:50:35 +0800

    send post request

---
+++ src/telegram/TelegramMethods.ts
@@ -0,0 +1,32 @@
+// Telegram Methods
+import axios from 'axios'
+import 'dotenv/config'
+
+const TELEGRAM_BOT_TOKEN = process.env.BOT_ID
+const TELEGRAM_CHAT_ID = process.env.CHAT_ID 
+
+export enum RequestMethods {
+  GET='GET',
+  POST='POST',
+  PUT='PUT',
+  DELETE='DELETE',
+}
+
+export async function sendTelegramMessage (message : string) {
+  const options = {
+    method: RequestMethods.POST,
+    url: `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+    data: {
+      "chat_id" : TELEGRAM_CHAT_ID,
+      "text" : message
+    }
+  }
+  axios
+  .request(options)
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
